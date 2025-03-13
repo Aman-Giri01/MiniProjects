d4087414a2988178c6eff4e94ca523398b666ce4
Aman-Giri01 <amankumargiri2022@gmail.com>
2025-03-13 14:12:46 +0530

    GitHub Account Finder Project

---
+++ public/projects/Git_Profile_Finder/script.js
@@ -0,0 +1,110 @@
+const img=document.querySelector('.img');
+const followers=document.querySelector('#followers');
+const follows=document.querySelector('#follows');
+const repo_details=document.querySelector('.repo_details');
+const btn=document.querySelector('#btn');
+const login=document.querySelector('#name');
+
+let user_name='';
+document.getElementById("formdata").addEventListener('submit',(event)=>{
+    event.preventDefault();
+    const formdata= new FormData(event.target);
+    input=formdata.get('username').trim();
+    if (input.length<=0)
+    {
+        alert("please Enter username");
+        
+        return;
+
+    }
+    else{
+        user_name=input.split("").join("");
+        fetchUser();
+    }
+});
+
+const fetchUser=()=>{
+
+    fetch(`https://api.github.com/users/${user_name}`)
+    .then((res)=>{
+        return res.json()})
+    .then((data)=>{
+        if(data.message==="Not Found"){
+            alert('user not found');
+            return false;
+        }else
+        {
+            img.innerHTML=`<img src="${data.avatar_url}">`;
+            login.innerHTML=data.login;
+            followers.innerHTML=data.followers;
+            follows.innerHTML=data.following;
+        }
+    }).catch(error => console.error("Error fetching data:", error));;
+
+    fetch(`https://api.github.com/users/${user_name}/repos`)
+    .then((res)=>{
+        return res.json()})
+    .then((repo_data)=>{
+        if(repo_data.length<=0)
+        {
+            repo_details.innerHTML=`
+             <div class="item">
+                        <div class="repo_name">Aman</div>
+             </div>                    `
+        }
+        else{
+            if(repo_data.message==="Not Found")
+            {
+                repo_details.innerHTML=`
+                <div class="item">
+                    <div class="repo_name">Aman</div>
+                        <div class="repo_detail">
+                            <div class="info_stars">
+                                <i class="fa fa-star"></i> 10
+                            </div>
+                            <div class="info_fork">
+                                <p><i class="fa fa-code-fork"></i> 30</p>
+                            </div>
+                            <div class="info_size">
+                            <p><i class="fa fa-file"></i> 30</p>
+                            </div>  
+                        </div>
+                    </div>
+                </div>
+                `
+                img.innerHTML=`<img src="">`;
+                login.innerHTML=`Aman`;
+                followers.innerHTML=`0`;
+                follows.innerHTML= `0`;
+            }
+            else{
+                let repo_Data=repo_data.map(item=>{
+                    return(
+                        `<div class="item">
+                            <div class="repo_name">${item.name}</div>
+                                <div class="repo_detail">
+                                    <div class="info_stars">
+                                        <i class="fa fa-star"></i> ${item.watchers}
+                                    </div>
+                                    <div class="info_fork">
+                                        <p><i class="fa fa-code-fork"></i> ${item.forks}</p>
+                                    </div>
+                                    <div class="info_size">
+                                    <p><i class="fa fa-file"></i> ${item.size} kb</p>
+                                    </div>  
+                                </div>
+                            </div>
+                        </div>
+                        `
+
+                    );
+                    
+
+
+                });
+                repo_details.innerHTML=repo_Data.slice(0,6).join("");
+            }
+        }
+    
+    });
+}
